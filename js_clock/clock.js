afe0f556d745e70672e7c0cf290efd8ea0b8c660
Anja Istenic <aistenic1@gmail.com>
2018-11-17 14:51:45 +0100

    added function for bg color change based on time of day

--- js_clock/clock.js
+++ js_clock/clock.js
@@ -1,6 +1,19 @@
 const hourHand = document.getElementById("hour-hand");
 const minuteHand = document.getElementById("min-hand");
 const secondHand = document.getElementById("second-hand");
+const container = document.querySelector(".clock-container");
+
+const change_container = () => {
+  const date = new Date;
+  const hour = date.getHours();
+  if (hour < 6 || hour > 20) {
+    container.style.backgroundColor = "#130a35";
+  } else if (hour < 12) {
+    container.style.backgroundColor = "#f6efff";
+  } else {
+    container.style.backgroundColor = "#3c008c";
+  }
+};
 
 const time = () => {
   const date = new Date;
@@ -13,6 +26,7 @@ const time = () => {
   minuteHand.style.transform = `rotate(${90 + 360/60*minute}deg)`;
   hourHand.style.transform = `rotate(${90 + 360/12*hour}deg)`;
   secondHand.style.transform = `rotate(${90 + 360/60*second}deg)`;
+  change_container();
 };
 
 setInterval(time, 1000);
